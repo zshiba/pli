0ec4ad097b6f2b3ae1929c05223303b9dbabf40b
zshiba <stream@wr-130-64-188-208.medford.tufts.edu>
2018-06-27 13:13:40 -0400

    Add the entry point

---
+++ src/PLI.java
@@ -0,0 +1,14 @@
+public class PLI{
+
+  public PLI(){
+  }
+
+  public void start(){
+  }
+
+  public static void main(String[] args){
+    System.out.println("=== PLI start ===");
+    new PLI().start();
+    System.out.println("=== PLI end ===");
+  }
+}
