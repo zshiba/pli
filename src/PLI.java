8b077f8da31fc3ca793d618ee39c6acd7eb2af11
zshiba <stream@wr-130-64-188-208.medford.tufts.edu>
2018-06-29 11:22:53 -0400

    Read input

--- src/PLI.java
+++ src/PLI.java
@@ -1,9 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class PLI{
+
 
   public PLI(){
   }
 
   public void loop(){
+    try(BufferedReader in = new BufferedReader(new InputStreamReader(System.in));){
+      StringBuilder builder = new StringBuilder();
+      int c;
+      while((c = in.read()) != -1){
+        builder.append((char)c);
+      }
+      String input = builder.toString();
+      System.out.println(input);
+    }catch(IOException e){
+      e.printStackTrace();
+    }
   }
 
   public static void main(String[] args){
