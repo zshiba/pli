9f411d11d8abdae1d26bc8d278dfadd7b9d9d082
zshiba <stream@wr-130-64-188-208.medford.tufts.edu>
2018-06-29 14:15:07 -0400

    Add lexer test suite

--- src/Lexer.java
+++ src/Lexer.java
@@ -10,4 +10,23 @@ public class Lexer{
     this.source = source;
   }
 
+  public Token getNextToken(){
+    return null;
+  }
+
+  public static void main(String[] args){
+    String source;
+    if(args.length > 0)
+      source = args[0];
+    else
+      source = "";
+
+    Lexer lexer = new Lexer(source);
+    Token token = null;
+    do{
+      token = lexer.getNextToken();
+      System.out.println(token.toString());
+    }while(token != null);
+  }
+
 }
--- src/Token.java
+++ src/Token.java
@@ -1,6 +1,6 @@
 public class Token{
 
-  public enum Type{
+  public static enum Type{
     OPEN_PARENTHESIS,
     CLOSE_PARENTHESIS,
     DOT,
@@ -9,4 +9,15 @@ public class Token{
 
   private Type type;
   private String literal;
+
+  public Token(Type type, String literal){
+    this.type = type;
+    this.literal = literal;
+  }
+
+  @Override
+  public String toString(){
+    return "{type: " + this.type.name() + ", literal: \"" + this.literal + "\"}";
+  }
+
 }
