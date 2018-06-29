b42dd722a90a45f42e8e4f51ae572c13fa5e7bfe
zshiba <stream@wr-130-64-188-208.medford.tufts.edu>
2018-06-29 15:54:55 -0400

    Update Lexer

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,6 +1,7 @@
 public class Lexer{
 
   private String source;
+  private int position;
 
   public Lexer(){
     this("");
@@ -8,10 +9,61 @@ public class Lexer{
 
   public Lexer(String source){
     this.source = source;
+    this.position = 0;
   }
 
   public Token getNextToken(){
-    return null;
+    this.skip();
+    Token token = null;
+    if(this.position < this.source.length()){
+      char character = this.source.charAt(this.position);
+      switch(character){
+        case '(':
+          token = new Token(Token.Type.OPEN_PARENTHESIS, "(");
+          ++this.position;
+          break;
+        case ')':
+          token = new Token(Token.Type.CLOSE_PARENTHESIS, ")");
+          ++this.position;
+          break;
+        case '.':
+          token = new Token(Token.Type.DOT, ".");
+          ++this.position;
+          break;
+        default:
+          String symbol = this.readSymbol();
+          token = new Token(Token.Type.SYMBOL, symbol);
+          break;
+      }
+      return token;
+    }else{
+      return token;
+    }
+  }
+
+  private void skip(){
+    for(int peek = this.position; peek < this.source.length(); peek++){
+      char c = this.source.charAt(peek);
+      if(c == ' ' || c == '\t' || c == '\n')
+        ++this.position;
+      else
+        break;
+    }
+  }
+
+  private String readSymbol(){
+    StringBuilder builder = new StringBuilder();
+    for(int peek = this.position; peek < this.source.length(); peek++){
+      char c = this.source.charAt(peek);
+      if(c == ' ' || c == '\t' || c == '\n' ||
+         c == '(' || c == ')' || c == '.'){
+        break;
+      }else{
+        builder.append(c);
+        ++this.position;
+      }
+    }
+    return builder.toString();
   }
 
   public static void main(String[] args){
@@ -22,6 +74,7 @@ public class Lexer{
       source = "";
 
     Lexer lexer = new Lexer(source);
+
     Token token = lexer.getNextToken();
     while(token != null){
       System.out.println(token.toString());
