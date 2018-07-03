b8fefe279efc35ba2e428c74bebb72f5c15988a7
zshiba <stream@wr-130-64-188-208.medford.tufts.edu>
2018-07-03 15:18:59 -0400

    Update Cell for empty list

--- src/Cell.java
+++ src/Cell.java
@@ -3,6 +3,10 @@ public class Cell implements SExpression{
   private SExpression car;
   private SExpression cdr;
 
+  public Cell(){
+    this(null, null);
+  }
+
   public Cell(SExpression car){
     this(car, Atom.NIL);
   }
@@ -12,13 +16,23 @@ public class Cell implements SExpression{
     this.cdr = cdr;
   }
 
+  public void cdr(SExpression expression){
+    this.cdr = expression;
+  }
+
+  public SExpression cdr(){
+    return this.cdr;
+  }
+
   @Override
   public String toString(){
-    String s = "( ";
-    s += this.car.toString();
-    s += " . ";
-    s += this.cdr.toString();
-    s += " )";
+    String s = "(";
+    if(this.car != null){
+      s += this.car.toString();
+      s += " . ";
+      s += this.cdr.toString();
+    }
+    s += ")";
     return s;
   }
 
