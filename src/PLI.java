327d759e790ee92c5c3a5b62360a7f13141c16be
zshiba <stream@wr-130-64-188-208.medford.tufts.edu>
2018-07-03 17:52:56 -0400

    Update REPL

--- src/PLI.java
+++ src/PLI.java
@@ -1,16 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class PLI{
 
-  private static final String PROMPT = "> ";
+  private static final String INPUT_PROMPT  = ">> ";
+  private static final String OUTPUT_PROMPT = "=> ";
+  private static final String ERROR_PROMPT  = "** ";
 
   public PLI(){
   }
 
-  private void read(BufferedReader in) throws IOException{
-    System.out.print(PROMPT);
+  private ArrayList<SExpression> read(BufferedReader in) throws IOException, Parser.ParseErrorException{
+    System.out.print(INPUT_PROMPT);
     StringBuilder builder = new StringBuilder();
     int c;
     while((c = in.read()) != -1){
@@ -21,12 +24,31 @@ public class PLI{
 
     Lexer lexer = new Lexer(source);
     Parser parser = new Parser(lexer);
+    ArrayList<SExpression> expressions = parser.parse();
+    return expressions;
+  }
+
+  private String evaluate(SExpression expression){
+    return ""; //ToDo
+  }
+
+  private void print(String prompt, String value){
+    System.out.print(prompt);
+    System.out.println(value);
   }
 
   public void loop(){
     try(BufferedReader in = new BufferedReader(new InputStreamReader(System.in));){
       while(true){
-        this.read(in);
+        try{
+          ArrayList<SExpression> expressions = this.read(in);
+          for(SExpression expression : expressions){
+            String value = this.evaluate(expression);
+            this.print(OUTPUT_PROMPT, value);
+          }
+        }catch(Parser.ParseErrorException pe){
+          this.print(ERROR_PROMPT, pe.getMessage());
+        }
       }
     }catch(IOException e){
       e.printStackTrace();
