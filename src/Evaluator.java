0f842ccaf8d5befc773f703170b3e112ec926023
zshiba <stream@wr-130-64-188-208.medford.tufts.edu>
2018-07-05 13:51:26 -0400

    Add Evaluator (WIP)

---
+++ src/Evaluator.java
@@ -0,0 +1,13 @@
+public class Evaluator{
+
+  private Environment environment;
+
+  public Evaluator(Environment environment){
+    this.environment = environment;
+  }
+
+  public String evaluate(SExpression expression){
+    return ""; //ToDo
+  }
+
+}
