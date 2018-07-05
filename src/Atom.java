0d4f87d04b9d31cf7a438fe7cdeee7a193294665
zshiba <stream@wr-130-64-188-208.medford.tufts.edu>
2018-07-05 10:33:25 -0400

    Add Atom.TRUE

--- src/Atom.java
+++ src/Atom.java
@@ -1,6 +1,7 @@
 public class Atom implements SExpression{
 
-  public static final Atom NIL = new Atom("nil");
+  public static final Atom NIL  = new Atom("nil");
+  public static final Atom TRUE = new Atom("t");
 
   private String symbol;
 
