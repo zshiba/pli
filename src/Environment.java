a605b211ea2d15613bc2fedf7926b08b58b338cb
zshiba <stream@wr-130-64-188-208.medford.tufts.edu>
2018-07-05 10:06:35 -0400

    Make Environment to be linked-list

--- src/Environment.java
+++ src/Environment.java
@@ -1,2 +1,13 @@
 public class Environment{
+
+  private Environment outer;
+
+  public Environment(){
+    this(null);
+  }
+
+  public Environment(Environment outer){
+    this.outer = outer;
+  }
+
 }
