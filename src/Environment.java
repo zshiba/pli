6846ba2e54b5cefb8b86e604d4c0ee8d7ec80a8c
zshiba <stream@wr-130-64-188-208.medford.tufts.edu>
2018-07-05 13:29:04 -0400

    Add environment.bind()

--- src/Environment.java
+++ src/Environment.java
@@ -1,6 +1,47 @@
 public class Environment{
 
+  private static class PairNode{ //node of association list (singly linked list of key-value pair)
+
+    private Atom key;
+    private SExpression value;
+    private boolean isPrimitive;
+    private PairNode next;
+
+    public PairNode(Atom key, SExpression value, boolean isPrimitive){
+      this.key = key;
+      this.value = value;
+      this.isPrimitive = isPrimitive;
+      this.next = null;
+    }
+
+    public Atom key(){
+      return this.key;
+    }
+
+    public SExpression value(){
+      return this.value;
+    }
+
+    public void setValue(SExpression value){
+      this.value = value;
+    }
+
+    public boolean isPrimitive(){
+      return this.isPrimitive;
+    }
+
+    public PairNode next(){
+      return this.next;
+    }
+
+    public void setNext(PairNode node){
+      this.next = node;
+    }
+
+  }
+
   private Environment outer;
+  private PairNode head;
 
   public Environment(){
     this(null);
@@ -8,6 +49,39 @@ public class Environment{
 
   public Environment(Environment outer){
     this.outer = outer;
+    this.head = null;
+  }
+
+  public boolean bind(Atom key, SExpression value){
+    return this.bind(key, value, false);
+  }
+
+  public boolean bind(Atom key, SExpression value, boolean isPrimitive){
+    boolean isBound = false;
+    PairNode newbie = new PairNode(key, value, isPrimitive);
+
+    PairNode previous = null;
+    PairNode current = this.head;
+    while(current != null){
+      if(current.key().equals(key))
+        break;
+      previous = current;
+      current = current.next();
+    }
+
+    if(previous == null && current == null){ //list was empty
+      this.head = newbie;
+      isBound = true;
+    }else if(previous != null && current == null){ //key has not been bound yet
+      previous.setNext(newbie);
+      isBound = true;
+    }else{
+      if(!current.isPrimitive()){
+        current.setValue(value); //key, which is non-primitive, already exits; overwrite value
+        isBound = true;
+      }
+    }
+    return isBound;
   }
 
 }
