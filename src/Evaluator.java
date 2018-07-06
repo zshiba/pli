35c380c2ef78716e95f8b29ae1f001679f67c462
zshiba <stream@wr-130-64-188-208.medford.tufts.edu>
2018-07-06 16:03:52 -0400

    Update Evaluater to handle quote

--- src/Evaluator.java
+++ src/Evaluator.java
@@ -10,12 +10,43 @@ public class Evaluator{
 
   public SExpression evaluate(SExpression expression, Environment environment) throws EvaluationErrorException{
     if(expression instanceof Atom){
-      SExpression e = environment.find((Atom)expression);
-      if(e == null)
-        throw new EvaluationErrorException("null");
-      return e;
+      Atom key = (Atom)expression;
+      SExpression e = environment.find(key);
+      if(e != null)
+        return e;
+      else
+        throw new EvaluationErrorException("Undefined: " + expression.toFullString());
+
+    }else if(expression instanceof Cell){
+      Cell cell = (Cell)expression;
+      if(cell.isEmpty()){
+        return Atom.NIL;
+
+      }else{
+        SExpression car = cell.car();
+        SExpression cdr = cell.cdr();
+
+        if(car.equals(Atom.QUOTE)){
+          return this.quote(cdr);
+        }else{
+          return null;
+        }
+      }
     }else{
-      return null; //ToDo
+      throw new EvaluationErrorException("Not implemented type: " + expression.getClass().getSimpleName());
+    }
+  }
+
+  //special form: (quote s-expression)
+  private SExpression quote(SExpression expression) throws EvaluationErrorException{
+    if(expression instanceof Cell){
+      Cell cell = (Cell)expression;
+      if(cell.cdr() == Atom.NIL)
+        return cell.car();
+      else
+        throw new EvaluationErrorException("Invalid expression: " + expression.toFullString());
+    }else{
+      throw new EvaluationErrorException("Invalid expression: " + expression.toFullString());
     }
   }
 
